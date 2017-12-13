92234f105ac03c36482486c3912387121bbc34ad
ketu.lai <ketu.lai@gmail.com>
2017-12-13 15:33:38 +0800

    update

--- wish/utils.py
+++ wish/utils.py
@@ -7,6 +7,7 @@ import json
 def build_response(resp):
     b = json.loads(resp.text)
     if resp.status_code == 200 and b["code"] == 0:
-        return b["data"]
-
-    raise AttributeError(b['message'])
+        if "data" in b:
+            return b["data"]
+        return b
+    raise ValueError(b['message'])
